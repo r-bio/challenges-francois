799c14ccd2e9d7e0f6a39b8a5f10a52d3e3d2736
Paolo Manca <paolomanca@dhcp-wlan-uzh-10-12-107-243.uzh.ch>
2018-09-06 09:08:49 +0200

    Added a kind comment on the code

--- cuke-challenge-francois.R
+++ cuke-challenge-francois.R
@@ -1,3 +1,5 @@
+#hey I'm Paolo, this is my first code modification from remote. Great thanks for the tutorial.
+
 ## Download data from course website
 download.file("http://r-bio.github.io/data/holothuriidae-specimens.csv",
               "data/holothuriidae-specimens.csv")
